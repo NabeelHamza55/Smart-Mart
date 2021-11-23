66266e9cba77290eece19943cfd66877ebd6402a
Nabeel Hamza <nabeelhamza55@gmail.com>
2021-11-23 21:09:58 +0500

    Update DB

--- Admin/function/db_connection.php
+++ Admin/function/db_connection.php
@@ -5,7 +5,7 @@ $password = "";
 
 // Create connection
 $db = new mysqli($servername, $username, $password);
-mysqli_select_db($db, 'smartmart2');
+mysqli_select_db($db, 'smartmart');
 
 // Check connection
 if ($db->connect_error) {
